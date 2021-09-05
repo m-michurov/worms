83671e8ad2d7a9a689055b47fda7ebba3139fe02
m-michurov <mikhail.michurov.00@gmail.com>
2021-09-06 00:45:05 +0700

    Test food creation in place occupied by a worm

--- WormsTest/Simulation/FoodTest.cs
+++ WormsTest/Simulation/FoodTest.cs
@@ -2,7 +2,9 @@ using NUnit.Framework;
 using Worms;
 using Worms.Food;
 using Worms.Names;
+using Worms.Utility;
 using WormsTest.Behaviour;
+using WormsTest.Food;
 using WormsTest.StateObserver;
 
 namespace WormsTest.Simulation {
@@ -40,6 +42,50 @@ namespace WormsTest.Simulation {
                     $"{nameof(i)} = {i}"
                 );
             }
+        }
+
+        [TestCase]
+        public void TestFoodCreatedOnWorm() {
+            var i = 0;
+            var s = new Worms.Simulation(
+                new NameGenerator(),
+                new DelegateFoodGenerator(_ => Vector2Int.UnitX * i++),
+                new DelegateBehaviour(
+                    (
+                        _,
+                        _
+                    ) => new Action.Nothing()
+                ),
+                new DiscardObserver()
+            );
+
+            var worm = s.TrySpawnWorm(Vector2Int.Zero)!;
+            const int steps = Worm.INITIAL_ENERGY / 2;
+            
+            s.Run(steps);
+            Assert.AreEqual(Worm.INITIAL_ENERGY - steps + Worm.ENERGY_PER_FOOD, worm.Energy);
+        }
+
+        [TestCase]
+        public void TestWormCanEatTwice() {
+            var i = 0;
+            var s = new Worms.Simulation(
+                new NameGenerator(),
+                new DelegateFoodGenerator(_ => Vector2Int.UnitX + -Vector2Int.UnitX * i++),
+                new DelegateBehaviour(
+                    (
+                        _,
+                        _
+                    ) => new Action.Move(Direction.Right)
+                ),
+                new DiscardObserver()
+            );
+
+            s.Run(1);
+            var worm = s.TrySpawnWorm(Vector2Int.Zero)!;
+            Assert.AreEqual(Worm.INITIAL_ENERGY, worm.Energy);
+            s.Run(1);
+            Assert.AreEqual(Worm.INITIAL_ENERGY - 1 + 2 * Worm.ENERGY_PER_FOOD, worm.Energy);
         }
     }
 }
