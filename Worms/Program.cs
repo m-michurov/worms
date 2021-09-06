2bdd6e8f280385491ccc70b81cb69d9abf1c3a97
m-michurov <mikhail.michurov.00@gmail.com>
2021-09-06 23:25:11 +0700

    Add an entry point

--- Worms/Program.cs
+++ Worms/Program.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.CompilerServices;
+using System.Threading;
+using Worms.Behaviour;
+using Worms.Food;
+using Worms.Names;
+using Worms.StateObserver;
+using Worms.Utility;
 
 [assembly: InternalsVisibleTo("WormsTest")]
 
 namespace Worms {
     internal static class Program {
-        private static void Main() => Console.WriteLine("Hello World!");
+        private const int STEPS = 100;
+        private static void Main() {
+            var snapshotCollector = new SnapshotCollector();
+            using var outFile = new FileStream("sim.out", FileMode.Create);
+            using var outWriter = new StreamWriter(outFile);
+            
+            var s = new Simulation(
+                new NameGenerator(),
+                new FoodGenerator(),
+                new HedonisticBehaviour(),
+                new MultiObserver(
+                    snapshotCollector,
+                    new TextStateWriter(outWriter))
+            );
+            _ = s.TrySpawnWorm(Vector2Int.Zero);
+            s.Run(STEPS);
+
+            foreach (var snapshot in snapshotCollector.Snapshots) {
+                Console.Clear();
+                Console.WriteLine(snapshot);
+                Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            }
+        }
     }
 }
