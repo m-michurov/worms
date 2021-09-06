2b1b93e3034106b560122fc5257fa429e81a3fa2
m-michurov <mikhail.michurov.00@gmail.com>
2021-09-06 22:38:26 +0700

    Test worm movement

--- WormsTest/Simulation/MovementTest.cs
+++ WormsTest/Simulation/MovementTest.cs
@@ -2,9 +2,8 @@ using NUnit.Framework;
 using Worms;
 using Worms.Names;
 using Worms.Utility;
-using WormsTest.Behaviour;
-using WormsTest.Food;
 using WormsTest.StateObserver;
+using WormsTest.TestImplementations;
 
 namespace WormsTest.Simulation {
     public sealed class MovementTest {
@@ -23,7 +22,7 @@ namespace WormsTest.Simulation {
             );
             var worm = s.TrySpawnWorm(Vector2Int.Zero)!;
             const int steps = Worm.INITIAL_ENERGY - 1;
-            
+
             s.Run(steps);
             Assert.AreEqual(new Vector2Int(steps, 0), worm.Position);
         }
@@ -51,10 +50,10 @@ namespace WormsTest.Simulation {
             var initialPosition = Vector2Int.Zero;
             var worm = s.TrySpawnWorm(initialPosition)!;
             _ = s.TrySpawnWorm(Vector2Int.UnitX);
-            
+
             const int steps = Worm.INITIAL_ENERGY - 1;
             Assert.IsTrue(steps > 0);
-            
+
             s.Run(steps);
             Assert.AreEqual(initialPosition, worm.Position);
         }
@@ -76,11 +75,14 @@ namespace WormsTest.Simulation {
 
             var worm = s.TrySpawnWorm(Vector2Int.Zero)!;
             Assert.AreEqual(Worm.INITIAL_ENERGY, worm.Energy);
-            
+
             const int steps = 1;
-            
+
             s.Run(steps);
-            Assert.AreEqual(Worm.INITIAL_ENERGY + Worm.ENERGY_PER_FOOD - steps, worm.Energy);
+            Assert.AreEqual(
+                Worm.INITIAL_ENERGY + Worm.ENERGY_PER_FOOD - steps * Worm.ENERGY_LOSS_PER_STEP,
+                worm.Energy
+            );
         }
     }
 }
