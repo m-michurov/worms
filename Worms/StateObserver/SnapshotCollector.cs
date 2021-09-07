177df0b8d2cb5f3e872053df0fcedb488fa1da2d
m-michurov <mikhail.michurov.00@gmail.com>
2021-09-07 21:53:44 +0700

    Encapsulate draw logic

--- Worms/StateObserver/SnapshotCollector.cs
+++ Worms/StateObserver/SnapshotCollector.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using Worms.Utility;
 
 namespace Worms.StateObserver {
@@ -9,19 +11,17 @@ namespace Worms.StateObserver {
 
         private const int MAX_X = 10;
         private const int MAX_Y = 10;
-        
-        private readonly List<string> snapshots = new();
 
-        internal IEnumerable<string> Snapshots => snapshots;
+        private readonly List<string> snapshots = new();
 
         public void StateChanged(ISimulationState s) {
             var snapshot = new StringBuilder();
-            
+
             snapshot.Append('+');
             snapshot.Append('-', (MAX_X - MIN_X + 1) * 2);
             snapshot.Append('+');
             snapshot.Append('\n');
-            
+
             for (var y = MIN_Y; y <= MAX_Y; y += 1) {
                 snapshot.Append('|');
                 for (var x = MIN_X; x <= MAX_X; x += 1) {
@@ -33,19 +33,31 @@ namespace Worms.StateObserver {
                     } else {
                         snapshot.Append(' ');
                     }
-                    
+
                     snapshot.Append(' ');
                 }
 
                 snapshot.Append('|');
                 snapshot.Append('\n');
             }
-            
+
             snapshot.Append('+');
             snapshot.Append('-', (MAX_X - MIN_X + 1) * 2);
             snapshot.Append('+');
-            
+
             snapshots.Add(snapshot.ToString());
+        }
+
+        internal void Show(TimeSpan stepDelay) {
+            Console.Clear();
+            
+            for (var i = 1; i <= snapshots.Count; i += 1) {
+                var snapshot = snapshots[i - 1];
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"Step #{i}");
+                Console.WriteLine(snapshot);
+                Thread.Sleep(stepDelay);
+            }
         }
     }
 }
