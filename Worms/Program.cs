0fbe47bdf1eab1b0ec3dca2c62c21ae55ed21938
m-michurov <mikhail.michurov.00@gmail.com>
2021-09-07 21:54:35 +0700

    Use command line parser

--- Worms/Program.cs
+++ Worms/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.CompilerServices;
-using System.Threading;
+using CommandLine;
 using Worms.Behaviour;
 using Worms.Food;
 using Worms.Names;
@@ -13,27 +13,57 @@ using Worms.Utility;
 namespace Worms {
     internal static class Program {
         private const int STEPS = 100;
-        private static void Main() {
-            var snapshotCollector = new SnapshotCollector();
-            using var outFile = new FileStream("sim.out", FileMode.Create);
-            using var outWriter = new StreamWriter(outFile);
-            
-            var s = new Simulation(
-                new NameGenerator(),
-                new FoodGenerator(),
-                new HedonisticBehaviour(),
-                new MultiObserver(
-                    snapshotCollector,
-                    new TextStateWriter(outWriter))
-            );
-            _ = s.TrySpawnWorm(Vector2Int.Zero);
-            s.Run(STEPS);
+        private const int DELAY_MS = 150;
+        private const string DEFAULT_OUTPUT_FILE = "sim.out";
 
-            foreach (var snapshot in snapshotCollector.Snapshots) {
-                Console.Clear();
-                Console.WriteLine(snapshot);
-                Thread.Sleep(TimeSpan.FromMilliseconds(150));
+        private static void Main(string[] args) =>
+            Parser.Default
+                .ParseArguments<Options>(args)
+                .WithParsed(Run);
+        
+        private static void Run(Options options) {
+            try {
+                using var outFile = new FileStream(options.OutputFile, FileMode.Create);
+                using var outWriter = new StreamWriter(outFile);
+
+                var snapshotCollector = null as SnapshotCollector;
+                var observer = new TextStateWriter(outWriter) as IStateObserver;
+
+                if (options.Visualize) {
+                    snapshotCollector = new SnapshotCollector();
+                    observer = new MultiObserver(observer, snapshotCollector);
+                }
+
+                var s = new Simulation(
+                    new NameGenerator(),
+                    new FoodGenerator(),
+                    new HedonisticBehaviour(),
+                    observer
+                );
+                _ = s.TrySpawnWorm(Vector2Int.Zero);
+                s.Run(STEPS);
+
+                snapshotCollector?.Show(TimeSpan.FromMilliseconds(DELAY_MS));
+            } catch (IOException e) {
+                Console.Error.WriteLine(e.Message);
             }
+        }
+
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private sealed class Options {
+            [Value(
+                0,
+                MetaName = nameof(OutputFile),
+                Default = DEFAULT_OUTPUT_FILE,
+                HelpText = "File to write simulation results to",
+                Required = false
+            )]
+            // ReSharper disable once PropertyCanBeMadeInitOnly.Local
+            public string OutputFile { get; set; } = DEFAULT_OUTPUT_FILE;
+
+            [Option(Default = false, HelpText = "Visualize simulation in CLI")]
+            // ReSharper disable once UnusedAutoPropertyAccessor.Local
+            public bool Visualize { get; set; }
         }
     }
 }
