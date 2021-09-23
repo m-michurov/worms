f33659a9ecccf617ca0d7e2b1e90259230e81666
m-michurov <mikhail.michurov.00@gmail.com>
2021-09-23 21:35:32 +0700

    Remove DI related stuff

--- Worms/Simulation.cs
+++ Worms/Simulation.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
 using Worms.Behaviour;
 using Worms.Food;
 using Worms.Names;
@@ -11,9 +8,7 @@ using Worms.StateObserver;
 using Worms.Utility;
 
 namespace Worms {
-    internal sealed class Simulation : IHostedService, ISimulationState {
-        private const int STEPS = 100;
-
+    internal sealed class Simulation : ISimulationState {
         private const int FOOD_DECAY_RATE = 1;
         internal const int FOOD_LIFETIME = 10;
         
@@ -147,10 +142,5 @@ namespace Worms {
                 foods.Remove(position);
             }
         }
-
-        public Task StartAsync(CancellationToken cancellationToken) =>
-            Task.Run(() => Run(STEPS), cancellationToken);
-
-        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
     }
 }
