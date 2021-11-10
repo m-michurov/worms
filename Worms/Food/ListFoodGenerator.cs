85a793de1b6312b5d7ef225792d28f761f848f41
m-michurov <mikhail.michurov.00@gmail.com>
2021-11-11 00:50:30 +0700

    Add ListFoodGenerator that uses provided collection of positions

---
+++ Worms/Food/ListFoodGenerator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Worms.Utility;
+
+namespace Worms.Food {
+    internal sealed class ListFoodGenerator : IFoodGenerator {
+        private readonly List<Vector2Int> foodPositions;
+        private int stepIndex;
+        
+        public ListFoodGenerator(IEnumerable<Vector2Int> foodPositions_) => foodPositions = foodPositions_.ToList();
+
+        public Vector2Int NextFoodPosition(Predicate<Vector2Int> isOccupied) {
+            if (stepIndex >= foodPositions.Count) {
+                throw new IndexOutOfRangeException("no more positions");
+            }
+            var position = foodPositions[stepIndex];
+            stepIndex += 1;
+
+            if (isOccupied(position)) {
+                throw new ArgumentException("positions collection is invalid");
+            }
+
+            return position;
+        }
+    }
+}
---
+++ WormsTest/Food/ListFoodGeneratorTests.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Worms.Food;
+using Worms.Utility;
+using Xunit;
+
+namespace WormsTest.Food {
+    public sealed class ListFoodGeneratorTests {
+        [Fact]
+        public void ListFoodGenerator_cannot_return_occupied_positions() {
+            // Arrange
+            var sut = new ListFoodGenerator(new[] {Vector2Int.Zero,});
+
+            var op = (Action) (() => sut.NextFoodPosition(_ => true));
+
+            // Assert
+            op.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ListFoodGenerator_returns_provided_positions_in_correct_order() {
+            // Arrange
+            var points = new[] {Vector2Int.Zero, Vector2Int.UnitX, Vector2Int.UnitY};
+            var sut = new ListFoodGenerator(points);
+            var retrieved = new List<Vector2Int>();
+            
+            // Act
+            for (var i = 0; i < points.Length; i += 1) {
+                retrieved.Add(sut.NextFoodPosition(point => retrieved.Contains(point)));
+            }
+            
+            // Assert
+            retrieved.Should().BeEquivalentTo(points);
+        }
+    }
+}
